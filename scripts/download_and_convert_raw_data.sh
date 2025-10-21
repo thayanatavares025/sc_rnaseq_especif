5c034bbc13c8f7362b4411c579bb7f64120e84dc
thayanatavares025 <thayana.tavares.025@ufrn.edu.br>
2025-10-21 01:12:24 -0300

    Update download_and_convert_raw_data.sh

--- scripts/download_and_convert_raw_data.sh
+++ scripts/download_and_convert_raw_data.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 sra_id=$1
-echo "Recebi ID: $sra_id"
 
-# Use pastas dentro do diretório atual (workdir)
 sra_dir="./sras"
 fastq_dir="./fastqs"
 
@@ -18,5 +16,4 @@ fasterq-dump "$sra_dir/$sra_id" -O "$fastq_dir" --split-files --force
 mv "$fastq_dir/${sra_id}_1.fastq" "$fastq_dir/${sra_id}_S0_L001_R1_001.fastq"
 mv "$fastq_dir/${sra_id}_2.fastq" "$fastq_dir/${sra_id}_S0_L001_R2_001.fastq"
 
-# Optional cleanup
 # rm -f "$sra_dir/$sra_id"
